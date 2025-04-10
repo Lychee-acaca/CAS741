70ac17363c11066a481fd43b6690abfd23f70d49
Jared Lin <lycknight@outlook.com>
2025-04-10 00:51:50 -0400

    remove main.cpp from coverage test

--- test/lcovTest.sh
+++ test/lcovTest.sh
@@ -7,10 +7,19 @@ cd "$(dirname "$0")/.."
 TMPDIR=$(mktemp -d)
 
 lcov -c -i -d . -o "$TMPDIR/base.info"
+
+echo "Collecting coverage data..."
+
 ./test/cTest.sh
+
+echo "Test execution completed."
+
 lcov -c -d . -o "$TMPDIR/test.info"
 lcov -a "$TMPDIR/base.info" -a "$TMPDIR/test.info" -o "$TMPDIR/total.info"
-lcov --extract "$TMPDIR/total.info" "$(pwd)/src/*" -o "$TMPDIR/final.info"
+
+lcov --extract "$TMPDIR/total.info" "$(pwd)/src/*" -o "$TMPDIR/extract.info"
+
+lcov --remove "$TMPDIR/extract.info" "$(pwd)/src/main.cpp" -o "$TMPDIR/final.info"
 
 rm -rf ./cover_report
 
